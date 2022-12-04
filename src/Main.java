a1e99ee027e38e4f4770dca00cfb9315e4297975
Alex <albredmy@gmail.com>
2022-12-04 22:34:43 +0300

    Задание №7

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ public class Main {
 
         var boxMen1= 78.2;
         var boxMen2= 82.7;
-        var totalWeight=boxMen1+boxMen2;
-        System.out.println("Общий вес боксеров " +totalWeight+" кг");
-        var diffWeight=boxMen2-boxMen1;
-        System.out.println("Разница в весе " +diffWeight+" кг");
+
+        var diffWeight1=boxMen2-boxMen1;
+        System.out.println("Разница в весе, способ №1 " +diffWeight1+" кг");
+        var diffWeight2=boxMen2 % boxMen1;
+        System.out.println("Разница в весе, способ №2 " +diffWeight2+" кг");
 
 
     }
