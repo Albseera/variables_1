8777b1b85d45b047e0736fb3392880b5d7576b49
Alex <albredmy@gmail.com>
2022-12-04 21:51:20 +0300

    Задание №6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        var frog= 3.5;
-        System.out.println(frog);
-        frog=frog*10;
-        System.out.println(frog);
-        frog=frog/3.5;
-        System.out.println(frog);
-        frog=frog+4;
-        System.out.println(frog);
+        var boxMen1= 78.2;
+        var boxMen2= 82.7;
+        var totalWeight=boxMen1+boxMen2;
+        System.out.println("Общий вес боксеров " +totalWeight+" кг");
+        var diffWeight=boxMen2-boxMen1;
+        System.out.println("Разница в весе " +diffWeight+" кг");
 
 
     }
